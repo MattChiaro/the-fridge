994328464a5bd5e893112eebe14286d58b565831
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-07-27 14:15:34 -0500

    server starts

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          How are ya now
         </a>
       </header>
     </div>
