efea02fb2788933360cae882e667da268264d085
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-07-28 13:50:12 -0500

    Bulletins

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,7 +1,7 @@
 const User = require('../models/User'); // User model may need to be updated
-
+const Bulletin = require('../models/Bulletin'); 
 const resolvers = {
-  Query: {
+  Query : {
     users: async () => {
       return await User.find({});
     },
@@ -9,6 +9,16 @@ const resolvers = {
       return await User.findById(_id);
     },
   },
+
+  Query : {
+    bulletins: async () => {
+      return await Bulletin.find({});
+    },
+    bulletin: async (_, { _id }) => {
+      return await Bulletin.findById(_id);
+    },
+  },
+  
   Mutation: {
     addUser: async (_, { name, email, password }) => {
       return await User.create({ name, email, password });
