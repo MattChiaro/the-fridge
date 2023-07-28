78f2d14870e45498c9c83175339d86016a29c41b
Drew Jordan <ajordan12@luc.edu>
2023-07-28 11:16:41 -0500

    user model and resolver start

---
+++ server/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+UserSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.isValidPassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
