a6bcef5531eceb3d0a4ad6615df029385df1e4bd
Drew Jordan <ajordan12@luc.edu>
2023-07-28 13:14:35 -0500

    es5

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,74 +1,74 @@
-// require('dotenv').config();
-// const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
-// module.exports = {
-//     authMiddleware: function ({ req, res, next }) {
-//         // allows token to be sent via req.body, req.query, or headers
-//         let token = req.body.token || req.query.token || req.headers.authorization;
+module.exports = {
+    authMiddleware: function ({ req, res, next }) {
+        // allows token to be sent via req.body, req.query, or headers
+        let token = req.body.token || req.query.token || req.headers.authorization;
 
-//         // We split the token string into an array and return actual token
-//         if (req.headers.authorization) {
-//             token = token.split(' ').pop().trim();
-//         }
+        // We split the token string into an array and return actual token
+        if (req.headers.authorization) {
+            token = token.split(' ').pop().trim();
+        }
 
-//         if (!token) {
-//             return res.status(401).send('Access denied. No token provided.');
-//         }
+        if (!token) {
+            return res.status(401).send('Access denied. No token provided.');
+        }
 
-//         // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
-//         try {
-//             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//             req.user = decoded;
-//             next();
-//         } catch {
-//             res.status(400).send('Invalid token.');
-//         }
+        // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = decoded;
+            next();
+        } catch {
+            res.status(400).send('Invalid token.');
+        }
 
-//         // return the request object so it can be passed to the resolver as `context`
-//         return req;
-//     },
-//     signToken: function ({ email, name, _id }) {
-//         const payload = { email, name, _id };
-//         return jwt.sign({ data: payload }, process.env.JWT_SECRET, { expiresIn: '2h' });
-//     },
-// };
+        // return the request object so it can be passed to the resolver as `context`
+        return req;
+    },
+    signToken: function ({ email, name, _id }) {
+        const payload = { email, name, _id };
+        return jwt.sign({ data: payload }, process.env.JWT_SECRET, { expiresIn: '2h' });
+    },
+};
 
 
 // ES6
-import dotenv from 'dotenv';
-import jwt from 'jsonwebtoken';
+// import dotenv from 'dotenv';
+// import jwt from 'jsonwebtoken';
 
-dotenv.config();
+// dotenv.config();
 
-const authMiddleware = ({ req, res, next }) => {
-    // allows token to be sent via req.body, req.query, or headers
-    let token = req.body.token || req.query.token || req.headers.authorization;
+// const authMiddleware = ({ req, res, next }) => {
+//     // allows token to be sent via req.body, req.query, or headers
+//     let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // We split the token string into an array and return actual token
-    if (req.headers.authorization) {
-        token = token.split(' ').pop().trim();
-    }
+//     // We split the token string into an array and return actual token
+//     if (req.headers.authorization) {
+//         token = token.split(' ').pop().trim();
+//     }
 
-    if (!token) {
-        return res.status(401).send('Access denied. No token provided.');
-    }
+//     if (!token) {
+//         return res.status(401).send('Access denied. No token provided.');
+//     }
 
-    // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    } catch {
-        res.status(400).send('Invalid token.');
-    }
+//     // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
+//     try {
+//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//         req.user = decoded;
+//         next();
+//     } catch {
+//         res.status(400).send('Invalid token.');
+//     }
 
-    // return the request object so it can be passed to the resolver as `context`
-    return req;
-};
+//     // return the request object so it can be passed to the resolver as `context`
+//     return req;
+// };
 
-const signToken = ({ email, name, _id }) => {
-    const payload = { email, name, _id };
-    return jwt.sign({ data: payload }, process.env.JWT_SECRET, { expiresIn: '2h' });
-};
+// const signToken = ({ email, name, _id }) => {
+//     const payload = { email, name, _id };
+//     return jwt.sign({ data: payload }, process.env.JWT_SECRET, { expiresIn: '2h' });
+// };
 
-export { authMiddleware, signToken };
+// export { authMiddleware, signToken };
--- server/models/User.js
+++ server/models/User.js
@@ -1,41 +1,5 @@
-// const mongoose = require('mongoose');
-// const bcrypt = require('bcrypt');
-
-// const UserSchema = new mongoose.Schema({
-//     name: {
-//         type: String,
-//         required: true
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     password: {
-//         type: String,
-//         required: true
-//     }
-// });
-
-// UserSchema.pre('save', async function(next) {
-//     if (!this.isModified('password')) {
-//         return next();
-//     }
-//     const salt = await bcrypt.genSalt(10);
-//     this.password = await bcrypt.hash(this.password, salt);
-// });
-
-// UserSchema.methods.isValidPassword = async function(password) {
-//     return await bcrypt.compare(password, this.password);
-// }
-
-// const User = mongoose.model('User', UserSchema);
-
-// module.exports = User;
-
-// es6
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -67,4 +31,40 @@ UserSchema.methods.isValidPassword = async function(password) {
 
 const User = mongoose.model('User', UserSchema);
 
-export default User;
+module.exports = User;
+
+// es6
+// import mongoose from 'mongoose';
+// import bcrypt from 'bcrypt';
+
+// const UserSchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         unique: true
+//     },
+//     password: {
+//         type: String,
+//         required: true
+//     }
+// });
+
+// UserSchema.pre('save', async function(next) {
+//     if (!this.isModified('password')) {
+//         return next();
+//     }
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+// });
+
+// UserSchema.methods.isValidPassword = async function(password) {
+//     return await bcrypt.compare(password, this.password);
+// }
+
+// const User = mongoose.model('User', UserSchema);
+
+// export default User;
