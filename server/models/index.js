9abaafaa5d77b3d4c88bcf1271f95fa85a64524c
Drew Jordan <ajordan12@luc.edu>
2023-07-28 14:08:07 -0500

    index update

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,5 @@
 const Bulletin = require('./Bulletin');
 const User = require('./User');
+const Calendar = require('./Calendar');
 
-module.exports = { Bulletin, User };
+module.exports = { Bulletin, User, Calendar };
