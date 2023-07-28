08e7e9ec5e3a06e7629d694e3dc51cc7072d2ca7
Ana Grisham <anaunique@icloud.com>
2023-07-28 12:22:52 -0700

    added routes and pages into APP js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,51 @@
 import "./App.css";
 import useMediaQuery from "./hooks/useMediaQuery";
 import {useEffect, useState} from "react";
+
 import Navbar from "./components/Navbar";
+
+import Calendar from './pages/calendar';
+import Home from './pages/home';
+import Post from './pages/post';
+import Profile from './pages/profile';
+
 
 function App() {
   
   const isMobile = useMediaQuery("(max-width: 768px)");
 
   return (
+
+    <>
+    <Router>
+      <Header />
+
+      <Routes>
+        <Route 
+          path= '/'
+          element = {<Home />}        
+        />
+        <Route 
+        path = '/calendar'
+        element = {<Calendar/>}
+        />
+        <Route 
+        path = '/post'
+        element = {<Post/>}
+        />
+        <Route 
+        path = '/profile'
+        element = {<Profile/>}
+        />
     <div className="App">
       {/* Nav Bar */}
       <div>
         <Navbar />
       </div>
     </div>
+    </Routes>
+    </Router>
+    </>
   );
 }
 
