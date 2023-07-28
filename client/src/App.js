210c1d7364d9bb0b0cbc23723804cd72f8a78252
Ana Grisham <anaunique@icloud.com>
2023-07-28 13:18:22 -0700

    added react DOM to app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import useMediaQuery from "./hooks/useMediaQuery";
 import {useEffect, useState} from "react";
