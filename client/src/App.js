1bd526cf658726840e0252b0b27af89fc980d802
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-07-31 11:23:47 -0500

    routing functioning

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,37 @@
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
-// import {useEffect, useState} from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {ApolloProvider, ApolloClient, InMemoryCache} from "@apollo/client";
 
+//components
+import Landing from "./components/Landing";
 
-// import Landing from "./components/Landing";
-
-
+//pages
 import Home from './pages/home';
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
 
   return (
-    <div className="App">
-      {/* <Landing /> */}
-    <Home />
-    
-    </div>
+    <ApolloProvider client={client}>
+    <Router>
+      <Routes>
+        <Route 
+        path="/" 
+        element={<Landing />} 
+        />
+        <Route 
+        path="/home"
+        element={<Home />}
+        />
+      </Routes>
+    </Router>
+    </ApolloProvider>
+    // <Landing />
   );
 }
 
