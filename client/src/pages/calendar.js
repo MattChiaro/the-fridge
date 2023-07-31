aabf76a49c3c54a37f8414c3f96f117f108c0bad
TKuhner <trevorkuhner29@gmail.com>
2023-07-31 08:44:02 -0500

    added calendar cdn to index.html

---
+++ client/src/pages/calendar.js
@@ -0,0 +1,26 @@
+
+
+const Calendar = () => {
+    <html lang='en'>
+  <head>
+    <meta charset='utf-8' />
+    
+    <script>
+
+      document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth'
+        });
+        calendar.render();
+      });
+
+    </script>
+  </head>
+  <body>
+    <div id='calendar'></div>
+  </body>
+</html>
+}
+
+export default Calendar;
