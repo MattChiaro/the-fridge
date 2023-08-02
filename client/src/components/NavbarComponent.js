f79a392595d778992ccb05b2fbdd654fd847022e
Ana Grisham <anaunique@icloud.com>
2023-08-02 10:56:18 -0700

    added function to home prof and stuff

--- client/src/components/NavbarComponent.js
+++ client/src/components/NavbarComponent.js
@@ -29,9 +29,6 @@ const NavbarComponent = () => {
         <img src={require("../assets/logo192inline.png")} alt="logo" />
         <div className="fridgeTitle"></div>
         <div>
-          <Link to="/profile">
-            <People size={48} color="black" />
-          </Link>
           <p>Hi, {user.name}</p>
         </div>
 
