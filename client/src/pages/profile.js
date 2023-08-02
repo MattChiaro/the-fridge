e480742f2585757f95a0e84afed15d58e7c35919
Ana Grisham <anaunique@icloud.com>
2023-08-01 17:21:27 -0700

    logout btn functionality

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -4,7 +4,6 @@ import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCar
 import {House} from 'react-bootstrap-icons';
 import {Link} from 'react-router-dom'
 
-
 export default function ProfileStatistics() {
   return (
     <div style={{ background: '#FFCC80'}}>
