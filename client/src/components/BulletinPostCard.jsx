9cd460de012d028228da7a7ca7d82f3de944cf2d
Drew Jordan <ajordan12@luc.edu>
2023-08-02 22:55:38 -0500

    fixed the bulletin posts

--- client/src/components/BulletinPostCard.jsx
+++ client/src/components/BulletinPostCard.jsx
@@ -1,5 +1,7 @@
 import { useQuery, gql } from '@apollo/client';
 import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 //GraphQL query. 
 const GET_BULLETINS = gql`
@@ -26,20 +28,24 @@ const BulletinPostCard = () => {
 
     return (
         <div className='m-5'>
-            {data.bulletins.map(bulletin => (
-                <Card className='postCard' key={bulletin._id}>
-                    <Card.Body >
-                        <Card.Title style={{textAlign:"center"}}>{bulletin.title}</Card.Title>
-                        <Card.Text>
-                            {bulletin.body}
-                        </Card.Text>
-                        <Card.Footer>
-                            {bulletin.user.name}
-                            {/* Date */}
-                        </Card.Footer>
-                    </Card.Body>
-                </Card>
-            ))}
+            <Row className='bulletins-container'>
+                {data.bulletins.map(bulletin => (
+                    <Col xs={12} sm={6} md={4} lg={3} className="mb-4">
+                        <Card className='postCard' key={bulletin._id}>
+                            <Card.Body >
+                                <Card.Title style={{textAlign:"center"}}>{bulletin.title}</Card.Title>
+                                <Card.Text>
+                                    {bulletin.body}
+                                </Card.Text>
+                                <Card.Footer>
+                                    {bulletin.user.name}
+                                    {/* Date */}
+                                </Card.Footer>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
         </div>
     )
 }
